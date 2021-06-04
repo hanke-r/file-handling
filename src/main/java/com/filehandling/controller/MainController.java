0be51c40f02a8cd4740e417809b58b1870164b4f
han-ker <hanjaeok17@gmail.com>
2021-06-05 00:35:05 +0900

    2021.06.05 nio File Upload 구현 - 로컬 경로(지정) - size 제한(설정x)

--- src/main/java/com/filehandling/controller/MainController.java
+++ src/main/java/com/filehandling/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.filehandling.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Controller
 public class MainController {
@@ -19,19 +24,29 @@ public class MainController {
 
     @PostMapping("/ioFile")
     public String fileSubmit(@RequestParam("uploadFile") MultipartFile multipartFile){
-
         try{
             String filename = multipartFile.getOriginalFilename();
             System.out.println(filename);
 
-            File file = new File("D:/" + filename);
+            File file = new File("D:"+ File.separator + filename);
             multipartFile.transferTo(file);
         } catch(IOException e){
             e.printStackTrace();
         }
 
         return "redirect:/";
-
     }
 
+    @PostMapping("nioFile")
+    public String nioFileSubmit(@RequestParam("nioUploadFile") MultipartFile multipartFile){
+        Path path = Paths.get("D:" + File.separator + StringUtils.cleanPath(multipartFile.getOriginalFilename()));
+
+        try{
+            Files.copy(multipartFile.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+        return "redirect:/";
+    }
 }
